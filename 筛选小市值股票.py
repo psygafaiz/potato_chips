a65150e9f6dce556c79f9f0e85fefa62f0655c8e
psygafaiz <369932260@qq.com>
2024-07-18 15:41:14 +0800

    test3

---
+++ 筛选小市值股票.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv(r'D:\Download\交易软件\客户端\client_test_516_20240618_C\database\需要数据.csv')
+
+#df1 = df[df['total_mv']<1000000]
+#df1.to_csv(r'D:\Download\交易软件\客户端\client_test_516_20240618_C\database\小市值股票代码.csv')
